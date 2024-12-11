16373eb7e2a8e193e201e0ad1241a73e02d49297
Hugo Ljungqvist <hulj07001@utb.vaxjo.se>
2024-12-11 13:12:05 +0100

    Add project files.

---
+++ nivå1_1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace nivå1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <=10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
